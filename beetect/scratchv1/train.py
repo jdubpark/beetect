d6997ad65edefab8da12da48e90899739861decf
Jong Won Park <jwparktom@gmail.com>
2020-05-05 03:16:51 -0400

    Fixed checkpoint name

--- beetect/scratchv1/train.py
+++ beetect/scratchv1/train.py
@@ -241,10 +241,10 @@ def get_transform(train=False):
     return ImgAugTransform(train)
 
 
-def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+def save_checkpoint(state, is_best, filename='checkpoint.pt'):
     torch.save(state, filename)
     if is_best:
-        shutil.copyfile(filename, 'model_best.pth.tar')
+        shutil.copyfile(filename, 'model_best.pt')
 
 
 class AverageMeter(object):
