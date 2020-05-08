f80e2cf751972957a884d5d5504d3ba99afafc9e
Jong Won Park <jwparktom@gmail.com>
2020-05-07 23:58:20 -0400

    tensorboard batch scalar log

--- beetect/scratchv1/train.py
+++ beetect/scratchv1/train.py
@@ -207,6 +207,7 @@ def train(train_loader, model, optimizer, lr_scheduler, epoch, device, args):
 
         if batch_idx % args.print_freq == 0:
             progress.display(batch_idx)
+            writer.add_scalar('batch loss (train)', loss, batch_idx+epoch*args.batch_size)
 
 
 def validate(val_loader, model, device, args):
