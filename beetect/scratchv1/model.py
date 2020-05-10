84b0a476b67aa1a3160c50a9c0816d907eeb1e74
Jong Won Park <jwparktom@gmail.com>
2020-05-10 19:19:32 -0400

    Reduced model image min size for bigger batch

--- beetect/scratchv1/model.py
+++ beetect/scratchv1/model.py
@@ -71,7 +71,7 @@ def resnet50_fpn(pretrained=True, num_classes=2, **kwargs):
 
     # out channels is already defined as 256
     # attach FasterRCNN head
-    model = FasterRCNN(backbone, num_classes=num_classes, min_size=600)
+    model = FasterRCNN(backbone, num_classes=num_classes, min_size=224)
     return model
 
     """
