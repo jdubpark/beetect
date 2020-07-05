7cf2873f9d376c1b205c61ac468cc76b44acc006
Jong Won Park <jwparktom@gmail.com>
2020-07-05 17:28:17 +0900

    Changed model repo

---
+++ beetect/model/__init__.py
@@ -0,0 +1,11 @@
+from .backbone import EfficientDetBackbone
+from .efficientdet import *
+from .efficientnet import EfficientNet
+from .loss import FocalLoss
+from .utils import (
+    GlobalParams,
+    BlockArgs,
+    BlockDecoder,
+    efficientnet,
+    get_model_params,
+)
